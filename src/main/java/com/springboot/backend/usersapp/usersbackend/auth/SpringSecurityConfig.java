4005b67b0f1cf5d317ef689dcf5d1c2dd1d55cde
Exe <exelcarrizo@gmail.com>
2024-08-02 00:16:58 -0300

    change security

--- src/main/java/com/springboot/backend/usersapp/usersbackend/auth/SpringSecurityConfig.java
+++ src/main/java/com/springboot/backend/usersapp/usersbackend/auth/SpringSecurityConfig.java
@@ -69,7 +69,7 @@ public class SpringSecurityConfig {
     CorsConfigurationSource configurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOriginPatterns(Arrays.asList("*"));
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "*"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
